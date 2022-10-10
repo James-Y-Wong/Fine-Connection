385f5a23d5230f9ad171492a60faabc5985a3c2f
James-Y-Wong <jamesyeuwong@gmail.com>
2022-10-10 16:49:07 -0400

    changed more product names

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -63,7 +63,7 @@ db.once('open', async () => {
       quantity: 100
     },
     {
-      name: 'Camera',
+      name: 'Gold',
       category: categories[2]._id,
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
@@ -72,7 +72,7 @@ db.once('open', async () => {
       quantity: 30
     },
     {
-      name: 'Tablet',
+      name: 'Silver',
       category: categories[2]._id,
       description:
         'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
@@ -81,7 +81,7 @@ db.once('open', async () => {
       quantity: 30
     },
     {
-      name: 'Tales at Bedtime',
+      name: 'Platinum',
       category: categories[3]._id,
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
